7a87a75035fec701fdec84c8f9cb49e05b7da9d3
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-08 21:34:14 +0200

    Add dummy data to App comp.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,26 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel  Lorenz",
+          phone: "0123 4567 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 6544 21",
+          email: "julie@localhost.com",
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
