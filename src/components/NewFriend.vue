1505f88149d34dc3db0ecff9bd3bf06220d28527
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-09 08:56:47 +0200

    Add NewFriend comp

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent>
+    <div>
+      <label for="name">Friend's Name</label>
+      <input type="text" id="name" />
+    </div>
+    <div>
+      <label for="email">Friend's Email</label>
+      <input type="email" id="email" />
+    </div>
+    <div>
+      <label for="phone">Friend's Phone Number</label>
+      <input type="number" />
+    </div>
+    <div>
+      <label for="favorite">Favorite Status</label>
+      <input type="radio" name="" id="favorite" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
